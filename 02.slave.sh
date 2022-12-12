bfcad291404f6f9e7ddb89f81caef4fbd2d5cbb9
Ha Ngoc Linh <49241285+harnetlinh@users.noreply.github.com>
2022-12-13 02:05:10 +0700

    Update 02.slave.sh

--- 02.slave.sh
+++ 02.slave.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 apt-get update -y
-hostnamectl set-hostname slave
-
-echo "slaveIP slave" >> /etc/hosts
+# configure the hosts file
 echo "masterIP master" >> /etc/hosts
-
+# disable swap memory
 swapoff -a
-
+# Docker installation
 wget -qO- https://get.docker.com/ | sh
-
+#Kubernetes installation
 modprobe br_netfilter
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-
 sysctl --system
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
-
+# ajoutez qch ici
 sleep 30 
-cp /home/ubuntu/automation_kubernetes_test/10-kubeadm.conf /etc/systemd/system/kubelet.service.d/ 
-sleep 10
+cp /home/ubuntu/10-kubeadm.conf /etc/systemd/system/kubelet.service.d/ 
+sleep 30
 systemctl daemon-reload
 sleep 30
-
-
+# connect
 rm /etc/containerd/config.toml
 systemctl restart containerd
