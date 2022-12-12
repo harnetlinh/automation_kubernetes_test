0306aaaddf57054cae5dc19ae9792139d62cd01f
Ha Ngoc Linh <linhhn13@fe.edu.vn>
2022-12-12 20:25:23 +0700

    up

---
+++ 01.master-02.sh
@@ -0,0 +1,6 @@
+#!/bin/bash#
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
--- shell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install python3-pip -y;
-pip install flask;
